08029dbc18b0028d79dbfa5bec1d4b3199af6b1b
Nayan Chakrabarty <nayandcc5@gmail.com>
2020-02-23 12:00:47 +0600

    adsfdsfasf

---
+++ app/src/main/java/com/nexttech/easybusinesscard/InfromationActivity.java
@@ -0,0 +1,14 @@
+package com.nexttech.easybusinesscard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfromationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infromation);
+    }
+}
