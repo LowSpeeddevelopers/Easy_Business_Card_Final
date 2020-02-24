69679071176cbe62aa15212b76f36a922ffab541
Shahariar Newaz Taki <43105661+shahariartaki@users.noreply.github.com>
2020-02-24 12:39:24 +0600

    qr code

--- app/src/main/java/com/nexttech/easybusinesscard/Fragments/ScanFragment.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Fragments/ScanFragment.java
@@ -1,17 +1,34 @@
 package com.nexttech.easybusinesscard.Fragments;
 
 
+import android.Manifest;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.nexttech.easybusinesscard.R;
 
 public class ScanFragment extends Fragment {
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
 
 
     public ScanFragment() {
@@ -22,8 +39,60 @@ public class ScanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        scannView = view.findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(getContext(),scannView);
+        resultData = view.findViewById(R.id.resultsOfQr);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+                    }
+                });
+
+            }
+        });
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
 
         return view;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requestForCamera();
+
+    }
+
+    public void requestForCamera() {
+        Dexter.withActivity(getActivity()).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(getContext(), "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+
+            }
+        }).check();
     }
 
 }
