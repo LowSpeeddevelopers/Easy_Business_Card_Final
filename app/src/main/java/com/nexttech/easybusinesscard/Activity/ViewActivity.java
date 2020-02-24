e40152a7f172168ed7068ae0d394eb1fa7e9fe65
HridoyHasan <hridoyhasan14@gmail.com>
2020-02-24 14:09:27 +0600

    Templates

--- app/src/main/java/com/nexttech/easybusinesscard/Activity/ViewActivity.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Activity/ViewActivity.java
@@ -3,14 +3,55 @@ package com.nexttech.easybusinesscard.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.nexttech.easybusinesscard.R;
 
 public class ViewActivity extends AppCompatActivity {
+    private ImageView imageFront, imageBack;
+    Button btnSave,btnShare;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        imageFront=findViewById(R.id.image_front);
+        imageBack=findViewById(R.id.image_back);
+        btnSave=findViewById(R.id.btnSave);
+        btnShare=findViewById(R.id.btnShare);
+
+        String temp=getIntent().getStringExtra("template");
+
+        if (temp.equals("temp1")){
+            imageFront.setImageDrawable(getResources().getDrawable(R.drawable.temp1_front));
+            imageBack.setImageDrawable(getResources().getDrawable(R.drawable.temp1_back));
+        } else if (temp.equals("temp2")){
+            imageFront.setImageDrawable(getResources().getDrawable(R.drawable.temp2_front));
+            imageBack.setImageDrawable(getResources().getDrawable(R.drawable.temp2_back));
+        } else if (temp.equals("temp3")){
+            imageFront.setImageDrawable(getResources().getDrawable(R.drawable.temp3_front));
+            imageBack.setImageDrawable(getResources().getDrawable(R.drawable.temp3_back));
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ViewActivity.this, "Save", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ViewActivity.this, "Share", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/nexttech/easybusinesscard/Fragments/TemplatesFragment.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Fragments/TemplatesFragment.java
@@ -1,6 +1,7 @@
 package com.nexttech.easybusinesscard.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,10 +10,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.nexttech.easybusinesscard.Activity.ViewActivity;
 import com.nexttech.easybusinesscard.R;
 
 public class TemplatesFragment extends Fragment {
+
+    private LinearLayout temp1, temp2, temp3;
 
 
     public TemplatesFragment() {
@@ -22,6 +27,38 @@ public class TemplatesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_templates, container, false);
+
+        temp1=view.findViewById(R.id.temp1);
+        temp2=view.findViewById(R.id.temp2);
+        temp3=view.findViewById(R.id.temp3);
+
+        temp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getContext(), ViewActivity.class);
+                i.putExtra("template","temp1");
+                startActivity(i);
+            }
+        });
+
+        temp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getContext(), ViewActivity.class);
+                i.putExtra("template","temp2");
+                startActivity(i);
+            }
+        });
+
+        temp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getContext(), ViewActivity.class);
+                i.putExtra("template","temp3");
+                startActivity(i);
+            }
+        });
+
 
         return view;
     }
