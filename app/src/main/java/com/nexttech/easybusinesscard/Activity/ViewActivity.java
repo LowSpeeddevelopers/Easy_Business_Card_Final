82c2f4223579eb2c45dd935f7f7f9e0bfec13b00
Afroz Hossain <afrozhossain97@gmail.com>
2020-02-25 12:15:58 +0600

    Card Design

--- app/src/main/java/com/nexttech/easybusinesscard/Activity/ViewActivity.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Activity/ViewActivity.java
@@ -1,22 +1,33 @@
 package com.nexttech.easybusinesscard.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nexttech.easybusinesscard.R;
+
+import java.util.zip.Inflater;
 
 public class ViewActivity extends AppCompatActivity {
 
     private ImageView imageFront, imageBack;
     Button btnSave,btnShare;
+
+    View dialogueView;
 
     Drawable imageResourceFront, imageResourceBack;
 
@@ -43,8 +54,45 @@ public class ViewActivity extends AppCompatActivity {
             imageResourceBack = getResources().getDrawable(R.drawable.temp3_back);
         }
 
-        imageFront.setImageDrawable(imageResourceFront);
+
+        dialogueView =getLayoutInflater().inflate(R.layout.activity_view, null);
+//        ImageView cardBackgroundFront;
+//        TextView cardName, cardDesignation, cardMobile;
+//        cardBackgroundFront = dialogueView.findViewById(R.id.card_front_background);
+//        cardName= dialogueView.findViewById(R.id.card_name);
+//        cardDesignation= dialogueView.findViewById(R.id.card_designation);
+//        cardMobile= dialogueView.findViewById(R.id.card_mobile);
+//
+//        cardBackgroundFront.setImageDrawable(imageResourceFront);
+//
+//        cardName.setText("Abcd");
+//        cardDesignation.setText("efgh");
+//        cardMobile.setText("23456");
+
+
+        dialogueView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                dialogueView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                dialogueView.getHeight(); //height is ready
+
+                Log.e("something","something");
+                Log.e("height",String.valueOf(dialogueView.getHeight()));
+                Log.e("width",String.valueOf(dialogueView.getWidth()));
+                imageFront.setImageBitmap(loadBitmapFromView(dialogueView));
+            }
+        });
+
+
+
         imageBack.setImageDrawable(imageResourceBack);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1000);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +114,7 @@ public class ViewActivity extends AppCompatActivity {
     }
 
     public Bitmap loadBitmapFromView(View v) {
-        Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap b = Bitmap.createBitmap(v.getMeasuredWidth(), v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
         v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
         v.draw(c);
