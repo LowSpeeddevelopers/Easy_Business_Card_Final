d16892d87dfdc64be068fc60e734be49bac9f7c0
HridoyHasan <hridoyhasan14@gmail.com>
2020-03-04 12:20:48 +0600

    Layout Fix

--- app/src/main/java/com/nexttech/easybusinesscard/Activity/MainActivity.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Activity/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
-
+ 
     @Override
     public void onBackPressed() {
         TemplatesFragment test = (TemplatesFragment) getSupportFragmentManager().findFragmentByTag("Templates");
